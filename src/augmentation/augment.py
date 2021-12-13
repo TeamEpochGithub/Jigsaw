66bdda911fd50fc1dda175ebd9c14f20df61c487
Patrik Barták <p.bartak@student.tudelft.nl>
2021-12-13 17:30:24 +0100

    Fix EDA reading data

--- src/augmentation/augment.py
+++ src/augmentation/augment.py
@@ -1,4 +1,5 @@
 from eda import *
+import pandas as pd
 
 #arguments to be parsed from command line
 import argparse
@@ -50,20 +51,21 @@ if alpha_sr == alpha_ri == alpha_rs == alpha_rd == 0:
 
 #generate more data with standard augmentation
 def gen_eda(train_orig, output_file, alpha_sr, alpha_ri, alpha_rs, alpha_rd, num_aug=9):
-
-    writer = open(output_file, 'w')
-    lines = open(train_orig, 'r').readlines()
-
-    for i, line in enumerate(lines):
-        parts = line[:-1].split('\t')
-        label = parts[0]
-        sentence = parts[1]
+    writer = open(output_file, 'w', encoding='utf8')
+    writer.write("comment_id,text\n")
+    df = pd.read_csv(train_orig)
+    print(f"Generating augmented sentences for {len(df.index)} rows")
+    num_new = 0
+    for index, row in df.iterrows():
+        label = row['comment_id']
+        sentence = row['text']
         aug_sentences = eda(sentence, alpha_sr=alpha_sr, alpha_ri=alpha_ri, alpha_rs=alpha_rs, p_rd=alpha_rd, num_aug=num_aug)
         for aug_sentence in aug_sentences:
-            writer.write(label + "\t" + aug_sentence + '\n')
-
+            num_new += 1
+            writer.write(f"{label},{aug_sentence}\n")
     writer.close()
-    print("generated augmented sentences with eda for " + train_orig + " to " + output_file + " with num_aug=" + str(num_aug))
+    print(f"Generated {num_new} sentences from {len(df.index)}")
+    print(f"Augmented sentences with EDA for {train_orig} to {output_file} with num_aug={num_aug}")
 
 #main function
 if __name__ == "__main__":
