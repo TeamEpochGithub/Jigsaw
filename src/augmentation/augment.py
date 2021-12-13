af049790fcfa8d953dc7ce0787cd3be2d53f4a71
Patrik Barták <p.bartak@student.tudelft.nl>
2021-12-13 19:57:12 +0100

    Add pipfile lock

--- src/augmentation/augment.py
+++ src/augmentation/augment.py
@@ -1,55 +1,59 @@
 from eda import *
 import pandas as pd
 
-#arguments to be parsed from command line
+# arguments to be parsed from command line
 import argparse
+
 ap = argparse.ArgumentParser()
 ap.add_argument("--input", required=True, type=str, help="input file of unaugmented data")
 ap.add_argument("--output", required=False, type=str, help="output file of unaugmented data")
 ap.add_argument("--num_aug", required=False, type=int, help="number of augmented sentences per original sentence")
-ap.add_argument("--alpha_sr", required=False, type=float, help="percent of words in each sentence to be replaced by synonyms")
+ap.add_argument("--alpha_sr", required=False, type=float,
+                help="percent of words in each sentence to be replaced by synonyms")
 ap.add_argument("--alpha_ri", required=False, type=float, help="percent of words in each sentence to be inserted")
 ap.add_argument("--alpha_rs", required=False, type=float, help="percent of words in each sentence to be swapped")
 ap.add_argument("--alpha_rd", required=False, type=float, help="percent of words in each sentence to be deleted")
 args = ap.parse_args()
 
-#the output file
+# the output file
 output = None
 if args.output:
     output = args.output
 else:
     from os.path import dirname, basename, join
+
     output = join(dirname(args.input), 'eda_' + basename(args.input))
 
-#number of augmented sentences to generate per original sentence
-num_aug = 9 #default
+# number of augmented sentences to generate per original sentence
+num_aug = 9  # default
 if args.num_aug:
     num_aug = args.num_aug
 
-#how much to replace each word by synonyms
-alpha_sr = 0.1#default
+# how much to replace each word by synonyms
+alpha_sr = 0.1  # default
 if args.alpha_sr is not None:
     alpha_sr = args.alpha_sr
 
-#how much to insert new words that are synonyms
-alpha_ri = 0.1#default
+# how much to insert new words that are synonyms
+alpha_ri = 0.1  # default
 if args.alpha_ri is not None:
     alpha_ri = args.alpha_ri
 
-#how much to swap words
-alpha_rs = 0.1#default
+# how much to swap words
+alpha_rs = 0.1  # default
 if args.alpha_rs is not None:
     alpha_rs = args.alpha_rs
 
-#how much to delete words
-alpha_rd = 0.1#default
+# how much to delete words
+alpha_rd = 0.1  # default
 if args.alpha_rd is not None:
     alpha_rd = args.alpha_rd
 
 if alpha_sr == alpha_ri == alpha_rs == alpha_rd == 0:
-     ap.error('At least one alpha should be greater than zero')
+    ap.error('At least one alpha should be greater than zero')
 
-#generate more data with standard augmentation
+
+# generate more data with standard augmentation
 def gen_eda(train_orig, output_file, alpha_sr, alpha_ri, alpha_rs, alpha_rd, num_aug=9):
     writer = open(output_file, 'w', encoding='utf8')
     writer.write("comment_id,text\n")
@@ -59,7 +63,8 @@ def gen_eda(train_orig, output_file, alpha_sr, alpha_ri, alpha_rs, alpha_rd, num
     for index, row in df.iterrows():
         label = row['comment_id']
         sentence = row['text']
-        aug_sentences = eda(sentence, alpha_sr=alpha_sr, alpha_ri=alpha_ri, alpha_rs=alpha_rs, p_rd=alpha_rd, num_aug=num_aug)
+        aug_sentences = eda(sentence, alpha_sr=alpha_sr, alpha_ri=alpha_ri, alpha_rs=alpha_rs, p_rd=alpha_rd,
+                            num_aug=num_aug)
         for aug_sentence in aug_sentences:
             num_new += 1
             writer.write(f"{label},{aug_sentence}\n")
@@ -67,8 +72,9 @@ def gen_eda(train_orig, output_file, alpha_sr, alpha_ri, alpha_rs, alpha_rd, num
     print(f"Generated {num_new} sentences from {len(df.index)}")
     print(f"Augmented sentences with EDA for {train_orig} to {output_file} with num_aug={num_aug}")
 
-#main function
-if __name__ == "__main__":
 
-    #generate augmented sentences and output into a new file
-    gen_eda(args.input, output, alpha_sr=alpha_sr, alpha_ri=alpha_ri, alpha_rs=alpha_rs, alpha_rd=alpha_rd, num_aug=num_aug)
+# main function
+if __name__ == "__main__":
+    # generate augmented sentences and output into a new file
+    gen_eda(args.input, output, alpha_sr=alpha_sr, alpha_ri=alpha_ri, alpha_rs=alpha_rs, alpha_rd=alpha_rd,
+            num_aug=num_aug)
--- src/augmentation/eda.py
+++ src/augmentation/eda.py
@@ -1,42 +1,44 @@
 import random
 from random import shuffle
+
 random.seed(1)
 
-#stop words list
-stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 
-            'ours', 'ourselves', 'you', 'your', 'yours',
-            'yourself', 'yourselves', 'he', 'him', 'his',
-            'himself', 'she', 'her', 'hers', 'herself',
-            'it', 'its', 'itself', 'they', 'them', 'their',
-            'theirs', 'themselves', 'what', 'which', 'who',
-            'whom', 'this', 'that', 'these', 'those', 'am',
-            'is', 'are', 'was', 'were', 'be', 'been', 'being',
-            'have', 'has', 'had', 'having', 'do', 'does', 'did',
-            'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or',
-            'because', 'as', 'until', 'while', 'of', 'at',
-            'by', 'for', 'with', 'about', 'against', 'between',
-            'into', 'through', 'during', 'before', 'after',
-            'above', 'below', 'to', 'from', 'up', 'down', 'in',
-            'out', 'on', 'off', 'over', 'under', 'again',
-            'further', 'then', 'once', 'here', 'there', 'when',
-            'where', 'why', 'how', 'all', 'any', 'both', 'each',
-            'few', 'more', 'most', 'other', 'some', 'such', 'no',
-            'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too',
-            'very', 's', 't', 'can', 'will', 'just', 'don',
-            'should', 'now', '']
+# stop words list
+stop_words = ['i', 'me', 'my', 'myself', 'we', 'our',
+              'ours', 'ourselves', 'you', 'your', 'yours',
+              'yourself', 'yourselves', 'he', 'him', 'his',
+              'himself', 'she', 'her', 'hers', 'herself',
+              'it', 'its', 'itself', 'they', 'them', 'their',
+              'theirs', 'themselves', 'what', 'which', 'who',
+              'whom', 'this', 'that', 'these', 'those', 'am',
+              'is', 'are', 'was', 'were', 'be', 'been', 'being',
+              'have', 'has', 'had', 'having', 'do', 'does', 'did',
+              'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or',
+              'because', 'as', 'until', 'while', 'of', 'at',
+              'by', 'for', 'with', 'about', 'against', 'between',
+              'into', 'through', 'during', 'before', 'after',
+              'above', 'below', 'to', 'from', 'up', 'down', 'in',
+              'out', 'on', 'off', 'over', 'under', 'again',
+              'further', 'then', 'once', 'here', 'there', 'when',
+              'where', 'why', 'how', 'all', 'any', 'both', 'each',
+              'few', 'more', 'most', 'other', 'some', 'such', 'no',
+              'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too',
+              'very', 's', 't', 'can', 'will', 'just', 'don',
+              'should', 'now', '']
 
-#cleaning up text
+# cleaning up text
 import re
-def get_only_chars(line):
 
+
+def get_only_chars(line):
     clean_line = ""
 
-    line = line.replace("’", "")
-    line = line.replace("'", "")
-    line = line.replace("-", " ") #replace hyphens with spaces
-    line = line.replace("\t", " ")
-    line = line.replace("\n", " ")
-    line = line.lower()
+    line = line.replace("’", "")  # replace backticks with spaces
+    line = line.replace("'", "")  # replace single quotes with spaces
+    line = line.replace("-", " ")  # replace hyphens with spaces
+    line = line.replace("\t", " ")  # replace tabs with spaces
+    line = line.replace("\n", " ")  # replace newlines with spaces
+    line = line.lower()  # lowercase all characters
 
     for char in line:
         if char in 'qwertyuiopasdfghjklzxcvbnm ':
@@ -44,20 +46,22 @@ def get_only_chars(line):
         else:
             clean_line += ' '
 
-    clean_line = re.sub(' +',' ',clean_line) #delete extra spaces
+    clean_line = re.sub(' +', ' ', clean_line)  # delete extra spaces
     if clean_line[0] == ' ':
         clean_line = clean_line[1:]
     return clean_line
+
 
 ########################################################################
 # Synonym replacement
 # Replace n words in the sentence with synonyms from wordnet
 ########################################################################
 
-#for the first time you use wordnet
+# for the first time you use wordnet
 # import nltk
 # nltk.download('wordnet')
 from nltk.corpus import wordnet
+
 
 def synonym_replacement(words, n):
     new_words = words.copy()
@@ -69,16 +73,17 @@ def synonym_replacement(words, n):
         if len(synonyms) >= 1:
             synonym = random.choice(list(synonyms))
             new_words = [synonym if word == random_word else word for word in new_words]
-            #print("replaced", random_word, "with", synonym)
+            # print("replaced", random_word, "with", synonym)
             num_replaced += 1
-        if num_replaced >= n: #only replace up to n words
+        if num_replaced >= n:  # only replace up to n words
             break
 
-    #this is stupid but we need it, trust me
+    # this is stupid but we need it, trust me
     sentence = ' '.join(new_words)
     new_words = sentence.split(' ')
 
     return new_words
+
 
 def get_synonyms(word):
     synonyms = set()
@@ -91,30 +96,31 @@ def get_synonyms(word):
         synonyms.remove(word)
     return list(synonyms)
 
+
 ########################################################################
 # Random deletion
 # Randomly delete words from the sentence with probability p
 ########################################################################
 
 def random_deletion(words, p):
-
-    #obviously, if there's only one word, don't delete it
+    # obviously, if there's only one word, don't delete it
     if len(words) == 1:
         return words
 
-    #randomly delete words with probability p
+    # randomly delete words with probability p
     new_words = []
     for word in words:
         r = random.uniform(0, 1)
         if r > p:
             new_words.append(word)
 
-    #if you end up deleting all words, just return a random word
+    # if you end up deleting all words, just return a random word
     if len(new_words) == 0:
-        rand_int = random.randint(0, len(words)-1)
+        rand_int = random.randint(0, len(words) - 1)
         return [words[rand_int]]
 
     return new_words
+
 
 ########################################################################
 # Random swap
@@ -127,17 +133,19 @@ def random_swap(words, n):
         new_words = swap_word(new_words)
     return new_words
 
+
 def swap_word(new_words):
-    random_idx_1 = random.randint(0, len(new_words)-1)
+    random_idx_1 = random.randint(0, len(new_words) - 1)
     random_idx_2 = random_idx_1
     counter = 0
     while random_idx_2 == random_idx_1:
-        random_idx_2 = random.randint(0, len(new_words)-1)
+        random_idx_2 = random.randint(0, len(new_words) - 1)
         counter += 1
         if counter > 3:
             return new_words
     new_words[random_idx_1], new_words[random_idx_2] = new_words[random_idx_2], new_words[random_idx_1]
     return new_words
+
 
 ########################################################################
 # Random insertion
@@ -150,55 +158,56 @@ def random_insertion(words, n):
         add_word(new_words)
     return new_words
 
+
 def add_word(new_words):
     synonyms = []
     counter = 0
     while len(synonyms) < 1:
-        random_word = new_words[random.randint(0, len(new_words)-1)]
+        random_word = new_words[random.randint(0, len(new_words) - 1)]
         synonyms = get_synonyms(random_word)
         counter += 1
         if counter >= 10:
             return
     random_synonym = synonyms[0]
-    random_idx = random.randint(0, len(new_words)-1)
+    random_idx = random.randint(0, len(new_words) - 1)
     new_words.insert(random_idx, random_synonym)
+
 
 ########################################################################
 # main data augmentation function
 ########################################################################
 
 def eda(sentence, alpha_sr=0.1, alpha_ri=0.1, alpha_rs=0.1, p_rd=0.1, num_aug=9):
-
     sentence = get_only_chars(sentence)
     words = sentence.split(' ')
-    words = [word for word in words if word is not '']
+    words = [word for word in words if word != '']
     num_words = len(words)
 
     augmented_sentences = []
-    num_new_per_technique = int(num_aug/4)+1
+    num_new_per_technique = int(num_aug / 4) + 1
 
-    #sr
+    # sr
     if alpha_sr > 0:
-        n_sr = max(1, int(alpha_sr*num_words))
+        n_sr = max(1, int(alpha_sr * num_words))
         for _ in range(num_new_per_technique):
             a_words = synonym_replacement(words, n_sr)
             augmented_sentences.append(' '.join(a_words))
 
-    #ri
+    # ri
     if alpha_ri > 0:
-        n_ri = max(1, int(alpha_ri*num_words))
+        n_ri = max(1, int(alpha_ri * num_words))
         for _ in range(num_new_per_technique):
             a_words = random_insertion(words, n_ri)
             augmented_sentences.append(' '.join(a_words))
 
-    #rs
+    # rs
     if alpha_rs > 0:
-        n_rs = max(1, int(alpha_rs*num_words))
+        n_rs = max(1, int(alpha_rs * num_words))
         for _ in range(num_new_per_technique):
             a_words = random_swap(words, n_rs)
             augmented_sentences.append(' '.join(a_words))
 
-    #rd
+    # rd
     if p_rd > 0:
         for _ in range(num_new_per_technique):
             a_words = random_deletion(words, p_rd)
@@ -207,14 +216,14 @@ def eda(sentence, alpha_sr=0.1, alpha_ri=0.1, alpha_rs=0.1, p_rd=0.1, num_aug=9)
     augmented_sentences = [get_only_chars(sentence) for sentence in augmented_sentences]
     shuffle(augmented_sentences)
 
-    #trim so that we have the desired number of augmented sentences
+    # trim so that we have the desired number of augmented sentences
     if num_aug >= 1:
         augmented_sentences = augmented_sentences[:num_aug]
     else:
         keep_prob = num_aug / len(augmented_sentences)
         augmented_sentences = [s for s in augmented_sentences if random.uniform(0, 1) < keep_prob]
 
-    #append the original sentence
+    # append the original sentence
     augmented_sentences.append(sentence)
 
     return augmented_sentences
