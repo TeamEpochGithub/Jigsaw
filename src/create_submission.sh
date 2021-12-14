09e50a70c4145abc291e39697f282639f07da88d
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2021-12-14 09:30:58 +0100

    move composite files to separate dir to avoid duplicate warning, mark the new dir as excluded

--- src/create_submission.sh
+++ src/create_submission.sh
@@ -1,2 +1,2 @@
-stickytape training/main.py --add-python-path ./training/ > training.py
-stickytape inference/main.py --add-python-path ./inference/ > inference.py
+stickytape training/main.py --add-python-path ./training/ > single_files/training.py
+stickytape inference/main.py --add-python-path ./inference/ > single_files/inference.py
